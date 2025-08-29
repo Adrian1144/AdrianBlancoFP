f7dfd3c1f8f2c96a940a90e72117f5987a658183
Adrian1144 <adrian.blancol@upb.edu.co>
2025-08-29 15:27:42 -0500

    Add files via upload
    While

---
+++ 9.CicloWhile/9.CicloWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _9.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acumulador = 0;
+
+           while (contador <= 5)
+           {
+                acumulador += contador;//acumulador=acumulador+contador
+                Console.WriteLine($"C: {contador} - A: {acumulador}");
+                contador++;
+           }
+
+            Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");
+        }
+    }
+}
