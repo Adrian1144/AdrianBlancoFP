e7ba5b1ca1a044a279e1afe384d1a0f90411bcb4
Adrian1144 <adrian.blancol@upb.edu.co>
2025-09-17 15:32:55 -0500

    Add files via upload
    Inicio clase Arreglos - Vectores

---
+++ 22.Vectores/22.Vectores/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _22.Vectores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Arreglos unidimensionales
+            //Vectores
+
+            int[] numeros = new int[5]; //new: reservar
+
+            string[] nombres = new string[4];
+
+            //Almacenamos datos
+            numeros[0] = 12;
+            numeros[1] = 32;
+            numeros[2] = 41;
+            numeros[3] = 100;
+            numeros[4] = 56;
+            //numeros[5] = 0; no se puede almacenar el dato poque la posición con índice 5 no existe.
+
+            //Recuperar
+            Console.WriteLine($"El valor almacenado en la posición 4, con índice 3 es: {numeros[3]}");
+
+            //Recorrer para almacenar
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ingrese el dato para la pocisión {i + 1}, con índice {i}");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Otras formas de inicializar vectores
+
+            char[] caracteres = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
+            float[] numeros2 = { 0.3f, 1.5f, 2.5f, 4.5f, 0.0f };
+
+            //Recorrer para recuperar datos almacenados
+
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                Console.WriteLine($"P:{i + 1} - I:{i}: {caracteres[i]}");
+            }
+
+            for (int i = 0; i < numeros2.Length; i++)
+            {
+                Console.WriteLine($"{numeros2[i]} |");
+            }
+
+
+            nombres[0] = "Adrian";
+            nombres[1] = "Roberto";
+            nombres[2] = "Alfredo";
+            nombres[3] = "Hernan";
+
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine($"Ingrese el nombre para la pocisión {i + 1}, con índice {i}");
+                nombres[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Posición " + i + ": " + nombres[i]);
+            }*/
+
+
+        }
+    }
+}
