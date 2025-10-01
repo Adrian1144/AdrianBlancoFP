6448808520fc7b152972ca5ce9cd5e57d50b8be8
Adrian1144 <adrian.blancol@upb.edu.co>
2025-10-01 06:53:05 -0500

    Add files via upload
    11.While

---
+++ 11.While/11.While/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _11.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //11. Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números ingresados. 
+
+            int contador = 0;
+            int suma = 0;
+            int n = 0;
+
+            while (n >= 0)
+            {
+                Console.WriteLine("Ingrese un número entero positivo:");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                suma += n;
+                contador++;
+            }
+
+            Console.WriteLine("La suma de los números ingreados es: " + suma + ".");
+
+        }
+    }
+}
