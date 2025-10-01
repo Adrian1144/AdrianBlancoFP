f53936edac9707169e87e4d7321ab93618f4db0e
Adrian1144 <adrian.blancol@upb.edu.co>
2025-10-01 06:53:35 -0500

    Add files via upload
    12.While

---
+++ 12.While/12.While/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _12.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //12.Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99.
+
+            int suma = 0;
+            int numero = 1;
+
+            while (numero <= 99)
+            {
+                suma += numero;
+                numero += 2;
+            }
+            Console.WriteLine("La suma de todos los números impares hasta 99 es: " + suma);
+        }
+    }
+}
